928afac3f750163e12193c675d71396b35bff888
khmhmm <khmhmm@ya.ru>
2021-11-20 00:52:46 +0400

    Updated oop with hashmap convertion

--- oop-prelude/src/macro_trait.rs
+++
@@ -1,5 +0,0 @@
-use crate::BoxedArg;
-
-pub trait Inherit<T>{
-    fn inherent_call(caller: &T, arg: BoxedArg);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,117 @@
 use std::collections::HashMap;
-use std::ops::Deref;
-use oop_prelude::*;
-use oop_macro::*;
 
-// #[derive(Inherit)]
-// pub struct Dog{}
-// impl Dog{
-//     fn f1(&self) {}
-// }
-
-
-
-struct Parent {
-    methods: HashMap<String, Box<Method<Parent>>>,
-    mut_methods: HashMap<String, Box<MethodMut<Parent>>>,
-    f: i32
+pub trait Class {
+    fn cast(map: HashMap<String,String>) -> Self where Self: Sized;
+    fn untyped(&self) -> HashMap<String, String>;
 }
 
-impl ClassDefinition for Parent {
-    fn get_methods() -> Vec<(String, Box<Method<Parent>>)> {
-        let mut v = Vec::<(String, Box<Method<Parent>>)>::new();
-        v.push(
-            ("show_name".to_string(),
-                {
-                    let f: BoxedRawMethod<Parent> =
-                        to_box!(|p, _| { println!("hello world"); to_box!( &() ) })
-                    ;
-                    Box::new(Method::<Parent>::from(f))
-                }
-            ));
-        v.push(
-            ("get_len".to_string(),
-                {
-                    let f: BoxedRawMethod<Parent> =
-                        to_box!(|p, _| { Box::new(p.f.clone()) })
-                    ;
-                    Box::new(Method::<Parent>::from(f))
-                }
-            ));
+pub mod inheritance {
+    use crate::Class;
 
-        v
-    }
+    pub struct Base;
 
-    fn get_mut_methods() -> Vec<(String, Box<MethodMut<Parent>>)> {
-        let mut v = Vec::<(String, Box<MethodMut<Parent>>)>::new();
-        v.push(
-            ("set_f".to_string(),
-                {
-                    let f: BoxedRawMethodMut<Parent> =
-                        to_box!(|p, f1| { p.f = format!("{:?}", f1).parse::<i32>().unwrap(); Box::new(()) })
-                    ;
-                    Box::new(MethodMut::<Parent>::from(f))
-                }
-            )
-        );
-        v
-    }
-}
+    impl Class for Base {
+        fn cast(map: std::collections::HashMap<String, String>) -> Self {
+            Self{}
+        }
 
-impl ClassConstructor for Parent {
-
-    fn construct() -> Parent {
-        Parent{
-            methods: Self::get_methods()
-                .into_iter()
-                .collect(),
-            mut_methods: Self::get_mut_methods()
-                .into_iter()
-                .collect(),
-            f: 1i32
+        fn untyped(&self) -> std::collections::HashMap<String, String> {
+            std::collections::HashMap::new()
         }
     }
 
-}
 
-impl ClassCalls for Parent {
-    fn call(&self, name: &str, arg: BoxedArg) -> BoxedReturn {
-        self.methods.get(name).expect(&format!("No such method: {}", name)).call(&self, arg)
-    }
-
-    fn call_mut(&mut self, name: &str, arg: BoxedArg) -> BoxedReturn {
-        // self.mut_methods.get_mut(name).expect(&format!("No such mut method: {}", name)).call(&mut self, arg)
-        to_box!(())
+    #[inline]
+    pub fn cast<F: Class, T: Class>(obj: &F) -> T {
+        T::cast(Class::untyped(obj))
     }
 }
 
-// impl std::ops::Index<&str> for Parent
-// {
-//     type Output = BoxedMember<Self>;
-//
-//     fn index(&self, i: &str) -> &BoxedMember<Self> {
-//         self.members.get(i).expect(&format!("No such method: {}", i))
-//     }
-// }
+
+
+struct Example {
+    field: i32
+}
+
+impl Example {
+    fn do_something(&self) -> i32 {
+        println!("Done something! {}", self.field);
+        self.field
+    }
+
+    fn base(&self) -> crate::inheritance::Base {
+        crate::inheritance::Base{}
+    }
+}
+
+impl Class for Example {
+    #[inline]
+    fn cast(map: HashMap<String, String>) -> Self {
+        Self {
+            field: map["field"].parse().unwrap()
+        }
+    }
+
+    fn untyped(&self) -> HashMap<String, String> {
+        let mut hm = HashMap::new();
+        hm.insert("field".to_string(), format!("{}", self.field));
+        return hm;
+    }
+}
+
+struct ExampleChild {
+// Parent's fields
+    field: i32,
+// Child's fields
+    data: String
+}
+
+impl ExampleChild {
+    fn new(field: i32, data: String) -> ExampleChild {
+        Self{field, data}
+    }
+
+    #[inline]
+    fn do_else(&self) -> String {
+        format!("{}:{}", self.field, self.data)
+    }
+
+    #[inline]
+    fn set_field(&mut self, field: i32) {
+        self.field = field;
+    }
+
+    #[inline]
+    fn base(&self) -> Example {
+        inheritance::cast::<ExampleChild, Example>(self)
+    }
+}
+
+impl Class for ExampleChild {
+    #[inline]
+    fn cast(map: HashMap<String, String>) -> Self {
+        Self {
+            field: map["field"].parse().unwrap(),
+            data: map["data"].clone()
+        }
+    }
+
+    fn untyped(&self) -> HashMap<String, String> {
+        let mut hm = HashMap::new();
+        hm.insert("field".to_string(), format!("{}", self.field));
+        hm.insert("data".to_string(), self.data.clone());
+        return hm;
+    }
+}
+
+
+
 
 fn main() {
-    let p = Parent::construct();
-    // call!(&p, "set_f", Box::new(5i32));
-    // println!( "{:?}", call!(&p, "get_len", Box::new(())) );
-
-    // let d = Dog{};
-    // Dog::inherent_call(&d, Box::new(()));
+    let mut child = ExampleChild::new(1,"hi".to_string());
+    let parent = inheritance::cast::<ExampleChild, Example>(&child);
+    parent.do_something();
+    child.set_field(2);
+    let base = child.base();
+    base.do_something();
 }
